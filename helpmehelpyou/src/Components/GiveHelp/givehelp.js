1d8e74186c18b72a6f742c54776de05a0a2897d3
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-06-08 15:00:20 +0100

    starting on skeleton for giveHelp

--- helpmehelpyou/src/Components/GiveHelp/givehelp.js
+++ helpmehelpyou/src/Components/GiveHelp/givehelp.js
@@ -1,2 +1,34 @@
-export default function GiveHelp() {}
+import React, { useState, useEffect } from "react";
 
+//create skeleton code for GiveHelp page.
+
+export default function GiveHelp() {
+  // state goes here
+
+  // functions: saving input from input box, click on button in listing to get email address of user.
+
+  /*renders:
+Header
+Nav Bar
+GiveHelpSearch - searchInput (input box) & searchBtn (button)
+Listing - <h1> for title / summary
+<p> for details
+<img> for profile picture
+<button> to close the component? */
+
+  return (
+    <div>
+      <section id="header"></section>
+      <section id="navBar"></section>
+      <section id="giveHelpSearch">
+        <input type="text"></input>
+        <button>Search</button>
+      </section>
+      <section id="listing">
+        <h1>Listing Title</h1>
+        <p>Listing Details</p>
+        <button>Contact user</button>
+      </section>
+    </div>
+  );
+}
