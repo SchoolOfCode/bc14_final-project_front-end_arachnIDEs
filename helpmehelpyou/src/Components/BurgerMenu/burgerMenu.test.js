325cebbff6f8eff1e67a200b0af7aee46b3c4842
Christopher Webb <webbophone@gmail.com>
2023-06-09 16:53:58 +0100

    First simple test complete in home.text.js. Ignore the failed test caused by there being nothing inside burgerMenu.test.js.

---
+++ helpmehelpyou/src/Components/BurgerMenu/burgerMenu.test.js
@@ -0,0 +1,5 @@
+// import { render, screen } from "@testing-library/react";
+// import { userEvent } from "@testing-library/user-event";
+// import { test, expect } from "@jest/globals";
+
+// This will fail as there's nothing in it. Just ignore that.
---
+++ helpmehelpyou/src/Components/Home/home.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+// import { userEvent } from "@testing-library/user-event";
+import { test, expect } from "@jest/globals";
+import HomePage from "./homePage";
+
+test("The title 'Home page!' should appear on the page.", () => {
+  render(<HomePage />);
+  //   screen.logTestingPlaygroundURL(); // a red line will appear underneath logTestingPlaygroundURL, but I ran the test anyway and the URL still appeared in the console.
+  //   const actual = screen.getBytext("Home page!");
+  const actual = screen.getByRole("heading", { name: /home page!/i }); // I got getByRole("heading", { name: /home page!/i }); by using screen.logTestingPlaygroundURL();
+  expect(actual).toBeInTheDocument();
+});
