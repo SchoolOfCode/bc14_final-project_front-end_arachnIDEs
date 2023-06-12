1570f609bbc5e32add6618b2661aa9ff326aea75
Sarah Salmean <sarahsalmean@hotmail.com>
2023-06-12 16:33:42 +0100

    working carousel on home page, updated sizing

--- helpmehelpyou/src/Components/Home/homePage.js
+++ helpmehelpyou/src/Components/Home/homePage.js
@@ -2,8 +2,24 @@ import "./homePage.css";
 import image from "./home_image.png";
 import logo from "./logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function HomePage() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const cards = [
+      { id: 1, title: "Card 1", content: "This is card 1" },
+      { id: 2, title: "Card 2", content: "This is card 2" },
+      { id: 3, title: "Card 3", content: "This is card 3" },
+    ];
+  
+    const handlePrev = () => {
+      setActiveIndex((prevIndex) => (prevIndex === 0 ? cards.length - 1 : prevIndex - 1));
+    };
+  
+    const handleNext = () => {
+      setActiveIndex((prevIndex) => (prevIndex === cards.length - 1 ? 0 : prevIndex + 1));
+    };
+
   return (
     <div className="home-page">
       <img className="app-logo" src={logo} alt="logo"></img>
@@ -19,23 +35,37 @@ export default function HomePage() {
         From moving flats to feeding cats - we have it covered.
       </h3>
       <div className="button-container">
-            <Link to = "/give">
-            <button className="home-page-buttons" id="give-help-button">
-          Give Help
-           </button> 
-           </Link>
-            <Link to = "/find">      
-              <button className="home-page-buttons" id="find-help-button">
-          Get Help
-        </button>
+        <Link to="/give">
+          <button className="home-page-buttons" id="give-help-button">
+            Give Help
+          </button>
+        </Link>
+        <Link to="/find">
+          <button className="home-page-buttons" id="find-help-button">
+            Get Help
+          </button>
         </Link>
       </div>
-
-      <div className ="carousel-container">
-      <div className = "home-page-card">
-       <h3>MEET NAME... </h3>
+      <div className="carousel">
+      <button className="left-arrow" onClick={handlePrev}>
+        ←
+      </button>
+      <div className="card">
+        <h2>{cards[activeIndex].title}</h2>
+        <p>{cards[activeIndex].content}</p>
       </div>
-      </div>
+      <button className="right-arrow" onClick={handleNext}>
+      →
+      </button>
+      <div className="dots">
+        {cards.map((card, index) => (
+          <span
+            key={card.id}
+            className={index === activeIndex ? "dot active" : "dot"}
+            onClick={() => setActiveIndex(index)}
+          ></span>
+        ))}
+      </div> </div>
     </div>
   );
 }
