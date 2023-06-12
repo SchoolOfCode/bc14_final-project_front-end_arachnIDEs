96d19822af6140e057950b2d06e8ac43e12b2c8a
olgaKhristo <olga84h@gmail.com>
2023-06-12 09:52:26 +0100

    added postRequestPage

--- helpmehelpyou/src/App.js
+++ helpmehelpyou/src/App.js
@@ -5,9 +5,10 @@ import HomePage from "./Components/Home/homePage";
 // to import Page not a componrnt(and giveHelpPage on line 18)
 import GiveHelpPage from "./pages/GiveHelp";
 import FindHelpPage from "./pages/FindHelp";
+
 import BurgerMenu from "./Components/BurgerMenu/burgerMenu";
 //import FindHelp from "./Components/FindHelp/findHelp";
-import PostRequest from "./Components/PostRequest/postRequest";
+import PostRequestPage from "./pages/PostRequest";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
           <Route path="/" element={<HomePage />} />        
           <Route path="/give" element={<GiveHelpPage />} />
           <Route path="/find" element={<FindHelpPage/>} />
-          <Route path="/findhelpform" element ={<PostRequest/>}/>
+          <Route path="/findhelpform" element ={<PostRequestPage/>}/>
         </Routes>
       </div>
     </>
