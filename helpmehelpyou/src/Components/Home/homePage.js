48c604d6184ed9aa4d1cd5ad4e44f835101a1b7c
Sarah Salmean <sarahsalmean@hotmail.com>
2023-06-12 14:20:10 +0100

    started home styling

--- helpmehelpyou/src/Components/Home/homePage.js
+++ helpmehelpyou/src/Components/Home/homePage.js
@@ -1,11 +1,22 @@
-import './homePage.css';
+import "./homePage.css";
+import image from "./home_image.png";
+import logo from "./logo.png";
 
 export default function HomePage() {
-
-
-    return <div className="homePage">
-    <h1 className="heading">
-    Home page
-    </h1>
+  return (
+    <div className="home-page">
+      <img className="app-logo" src={logo} alt="logo"></img>
+      <img
+        className="home-image"
+        src={image}
+        alt="cartoon person showing someone how to do something"
+      ></img>
+      <h3 className="intro-text">
+        Connect with your local community to give and receive favours, skills,
+        and support. <br></br>
+        <br></br>
+        From moving flats to feeding cats - we have it covered.
+      </h3>
     </div>
+  );
 }
