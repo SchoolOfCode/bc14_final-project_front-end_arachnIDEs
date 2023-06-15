6ec901e055be75ea5a46a4ef762b14f254f95534
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-06-15 13:35:48 +0100

    removed useContext from two tests

--- helpmehelpyou/src/Components/GiveHelp/giveHelp.test.js
+++ helpmehelpyou/src/Components/GiveHelp/giveHelp.test.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { render, screen, waitFor } from "@testing-library/react";
 // import { userEvent } from "@testing-library/user-event";
 import { test, expect } from "@jest/globals";
