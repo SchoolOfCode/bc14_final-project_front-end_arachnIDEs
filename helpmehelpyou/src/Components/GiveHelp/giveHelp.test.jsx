94a7ce94e181f75c8d2b3e1021c052311c9e16c6
Christopher Webb <webbophone@gmail.com>
2023-06-23 10:05:13 +0100

    cleaning up yellow underline.

--- helpmehelpyou/src/Components/GiveHelp/giveHelp.test.jsx
+++ helpmehelpyou/src/Components/GiveHelp/giveHelp.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 // import { userEvent } from "@testing-library/user-event";
 import { test, expect } from "@jest/globals";
 import GiveHelp from "./giveHelp";
